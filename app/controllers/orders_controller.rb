008f1e84b5b01df1713ced962942068997aa261f
Pham Quoc Duy <duypham@tinypulse.com>
2017-12-17 21:14:11 +0700

    ask price and coloring

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,9 @@
 class OrdersController < ApplicationController
   def index
     @orders = Bittrex::Order.open
+    @quotes = {}
+    @orders.each do | order |
+      @quotes[order.exchange] = Bittrex::Quote.current(order.exchange)
+    end
   end
 end
