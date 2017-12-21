a8b1224b5f94320593acec374670dd01c2c36009
Pham Quoc Duy <duypham@tinypulse.com>
2017-12-21 17:20:35 +0700

    add more info to list and coloring

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,22 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Bittrex::Order.open
+    @orders = Bittrex::Order.open.sort { | a, b | b.opened_at <=> a.opened_at }
+    @history = Bittrex::Order.history
+    @perks = {}
     @quotes = {}
     @orders.each do | order |
-      @quotes[order.exchange] = Bittrex::Quote.current(order.exchange)
+      buy = @history.detect{ | h | h.exchange == order.exchange && (h.quantity - order.quantity).abs < 0.000001 && h.type == 'Limit_buy' }
+      unless @quotes[order.exchange]
+        @quotes[order.exchange] = Bittrex::Quote.current(order.exchange)
+      end
+      quote = @quotes[order.exchange]
+      delta = buy ? ((quote.ask - buy.limit) / buy.limit * 100).round(2) : 0
+      @perks[order.id] = {
+        ask: '%.8f' % quote.ask,
+        buy: buy && '%.8f' % buy.limit,
+        delta_percentage: delta > 0 ? '(+%.2f%%)' % delta : '(%.2f%%)' % delta,
+        delta: delta
+      }
     end
   end
 end
