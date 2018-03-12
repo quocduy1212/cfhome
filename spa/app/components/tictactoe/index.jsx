650e3ae839c8bbef22289aa41176e5c38482d680
Pham Quoc Duy <pham@niometrics.com>
2018-03-12 18:13:51 +0700

    dn instead of rerender

--- spa/app/components/tictactoe/index.jsx
+++ spa/app/components/tictactoe/index.jsx
@@ -3,18 +3,14 @@ import { connect } from 'react-redux';
 import IndicatorsLayout from './indicators-layout';
 import BookmarksLayout from './bookmarks-layout';
 import AhihiLayout from './ahihi-layout';
-import GuessLayout from './guess-layout';
 
-const TicTacToe = ({ page }) => {
-  if (page === 'indicators') {
-    return <IndicatorsLayout />;
-  } else if (page === 'bookmarks') {
-    return <BookmarksLayout />;
-  } else if (page === 'ahihi') {
-    return <AhihiLayout />;
-  }
-  return <GuessLayout />;
-};
+const TicTacToe = ({ page }) => (
+  <div>
+    <IndicatorsLayout className={page === 'indicators' ? 'db' : 'dn'} />
+    <BookmarksLayout className={page === 'bookmarks' ? 'db' : 'dn'} />
+    <AhihiLayout className={page === 'ahihi' ? 'db' : 'dn'} />
+  </div>
+);
 
 const mapStateToProps = ({ settings }) => ({
   ...settings,
